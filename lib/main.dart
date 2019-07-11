463156daf29c5963ebd5097725c9ea1aabc53e56
souza.leandror@gmail.com <souza.leandror@gmail.com>
2019-07-11 17:47:14 -0300

    Commit Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,20 +8,101 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: Splash(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          child: FlareActor(
+            "assets/Gear.flr",
+            animation: "spin",
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3)).then((_) {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => Home()));
+    });
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            "Flutter + Flare",
+            style: TextStyle(fontSize: 30),
+            textAlign: TextAlign.center,
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            child: FlareActor(
+              "assets/Heart.flr",
+              animation: "pulse",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  String _anim = "spin1";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (_anim == "spin1")
+              _anim = "spin2";
+            else
+              _anim = "spin1";
+          });
+        },
+        child: Container(
+          width: 200,
+          height: 200,
+          child: FlareActor("assets/Gear.flr", animation: _anim, isPaused: true,),
+        ),
+      ),
+    ));
   }
 }
+*/
